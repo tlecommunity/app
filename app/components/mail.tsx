b64620c7f03f990a50c889b3ff8625ee6e55c91d
Natalie McCallum <natalie@omg.lol>
2025-08-12 15:12:52 +1000

    Window fixes

--- app/components/mail.tsx
+++ app/components/mail.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react';
 import MailWindowStore from 'app/stores/window/mail';
 
 import YAHOO from 'app/shims/yahoo';
 
-class MailWindow extends React.Component {
-  componentDidUpdate() {
+const MailWindow = function () {
+  useEffect(() => {
     if (MailWindowStore.shown) {
       YAHOO.lacuna.Messaging.show();
     }
-  }
+  }, [MailWindowStore.shown]);
 
-  render() {
-    const { shown } = MailWindowStore;
-    return <div />;
-  }
-}
+  return <div />;
+};
 
 export default observer(MailWindow);
--- app/components/options.tsx
+++ app/components/options.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react';
 import OptionsWindowStore from 'app/stores/window/options';
 
 import YAHOO from 'app/shims/yahoo';
 
-class OptionsWindow extends React.Component {
-  render() {
+const OptionsWindow = function () {
+  useEffect(() => {
     if (OptionsWindowStore.shown) {
       YAHOO.lacuna.Profile.show();
     }
+  }, [OptionsWindowStore.shown]);
 
-    return <div />;
-  }
-}
+  return <div />;
+};
 
 export default observer(OptionsWindow);
